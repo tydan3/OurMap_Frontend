95ec52279352fad449b12d69d207425655c33f74
Daniel Ty <tydaniel270@gmail.com>
2021-12-13 05:30:28 -0800

    created node rest api

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
+const pinRoute = require("./routes/pins");
 
 dotenv.config();
+
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -11,6 +14,8 @@ mongoose
     console.log("MongoDB Connected!");
   })
   .catch((err) => console.log(err));
+
+app.use("/api/pins", pinRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
