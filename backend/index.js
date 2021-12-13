5e00fe366aa5c260f332f8d45908e005d46b3129
Daniel Ty <tydaniel270@gmail.com>
2021-12-13 06:19:30 -0800

    created login and register api

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
 const pinRoute = require("./routes/pins");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use("/api/pins", pinRoute);
+app.use("/api/users", userRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
