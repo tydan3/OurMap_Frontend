3ae86d4cc510c50adb3c9b17a5c6e11a3f734d9d
Daniel Ty <tydaniel270@gmail.com>
2021-12-14 23:01:40 -0800

    Added MapBox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,24 @@
+import { useState } from "react";
+import ReactMapGL from "react-map-gl";
+
 function App() {
-  return <div className="App">hello world</div>;
+  const [viewport, setViewport] = useState({
+    width: "100vw",
+    height: "100vh",
+    latitude: 47.24459153284515,
+    longitude: -122.43792518560639,
+    zoom: 12,
+  });
+
+  return (
+    <div className="App">
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      />
+    </div>
+  );
 }
 
 export default App;
