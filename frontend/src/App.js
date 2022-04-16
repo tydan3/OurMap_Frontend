8952e181176719f2cde9699304ca1c0353bfb14c
Daniel Ty <tydaniel270@gmail.com>
2022-04-15 23:53:57 -0700

    Added popup example

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import ReactMapGL, { Marker } from "react-map-gl";
-import { Room } from "@material-ui/icons";
-import { red } from "@material-ui/core/colors";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { Room, Star } from "@material-ui/icons";
+import "./app.css";
 
 function App() {
   const [viewport, setViewport] = useState({
@@ -23,6 +23,33 @@ function App() {
         <Marker longitude={-122.4379} latitude={47.2445} anchor="bottom">
           <Room style={{ fontSize: viewport.zoom * 3, color: "Red" }}> </Room>
         </Marker>
+        <Popup
+          longitude={-122.4379}
+          latitude={47.2445}
+          closeButton={true}
+          closeOnClick={false}
+          anchor="bottom"
+        >
+          <div className="card">
+            <label>Place</label>
+            <h4 className="place">uwt</h4>
+            <label>Review</label>
+            <p className="desc">cool school</p>
+            <label>Rating</label>
+            <div className="stars">
+              <Star className="star" />
+              <Star className="star" />
+              <Star className="star" />
+              <Star className="star" />
+              <Star className="star" />
+            </div>
+            <label>Information</label>
+            <span className="username">
+              Created by <b>Dan</b>{" "}
+            </span>
+            <span className="date">1 hour ago</span>
+          </div>
+        </Popup>
       </ReactMapGL>
     </div>
   );
