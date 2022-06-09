61ee46966ad47edf36f7bd55f1ab903e9dd59c46
Daniel Ty <tydaniel270@gmail.com>
2022-06-08 18:49:16 -0700

    changed how-to description

--- frontend/src/components/Howto.jsx
+++ frontend/src/components/Howto.jsx
@@ -10,11 +10,11 @@ export default function Howto({ setShowHowto }) {
       </div>
       <div className="textContainer">
         <h3>Welcome and thank you for checking out my app!</h3>
-        <h4>Here are some things you can currently do:</h4>
+        <h4>Here are some things you can do:</h4>
         <p>
           <span>View pins -</span> Select any pin on the map to view its info.
-          Red pins are pins created by your user account, orange are pins
-          created by other user accounts. <br />
+          Red pins are pins created by your user account; orange pins are pins
+          created by other users. <br />
           <br />
           <span>Create pins -</span> Double-click (or double-press for mobile)
           anywhere on the map to start creating a pin for that location. Pins
@@ -27,10 +27,18 @@ export default function Howto({ setShowHowto }) {
           will be used for signing in. Your email will be used for a future
           password recovery feature. <br />
           <br />
-          <span>Sign in-</span> Select the "Sign in" button in the top right to
+          <span>Sign in -</span> Select the "Sign in" button in the top right to
           sign in to a registered account. You will be asked to enter your
           username and password. When signed in, pins you create will be
           associated to your username and be colored red.
+          <br /> <br />
+          <span>Delete your pin -</span> Feature to be added. Currently, you
+          cannot delete your pins, so be sure of the pins you create!
+          <br /> <br />
+          <span className="sub">
+            For assistance, please feel open to contacting me: text +1 (253)
+            391-0447 or email tydan.wk@gmail.com.
+          </span>
         </p>
       </div>
       <Cancel className="howtoCancel" onClick={() => setShowHowto(false)} />
