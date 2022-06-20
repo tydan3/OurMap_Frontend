cad74f4239bb8f4a1e89f1b2c057d855222d11ab
Daniel Ty <tydaniel270@gmail.com>
2022-06-19 17:31:16 -0700

    changed backend url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ import Signin from "./components/Signin";
 import Howto from "./components/Howto";
 
 function App() {
-  const myUrl = "http://localhost:8800"; // http://localhost:8800 : https://ourmapserver.click
+  const myUrl = "https://ourmapserver.click"; // http://localhost:8800 : https://ourmapserver.click
   const myStorage = window.localStorage;
   const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX;
 
